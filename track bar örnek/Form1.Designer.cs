4699e83e229dc64e9941fc9e82b887198339ffdc
ilayda yalçın <146716704+ilaydayacin@users.noreply.github.com>
2023-10-02 16:31:27 +0300

    Add files via upload

---
+++ track bar örnek/Form1.Designer.cs
@@ -0,0 +1,128 @@
+﻿namespace track_bar_örnek
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tbTaksit = new System.Windows.Forms.TrackBar();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtMiktar = new System.Windows.Forms.TextBox();
+            this.lblSonuc = new System.Windows.Forms.Label();
+            this.btnHesapla = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.tbTaksit)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // tbTaksit
+            // 
+            this.tbTaksit.Location = new System.Drawing.Point(52, 27);
+            this.tbTaksit.Maximum = 12;
+            this.tbTaksit.Minimum = 1;
+            this.tbTaksit.Name = "tbTaksit";
+            this.tbTaksit.Size = new System.Drawing.Size(322, 45);
+            this.tbTaksit.TabIndex = 0;
+            this.tbTaksit.Value = 1;
+            this.tbTaksit.ValueChanged += new System.EventHandler(this.tbTaksit_ValueChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.SystemColors.Control;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(52, 107);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(53, 24);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Tutar";
+            // 
+            // txtMiktar
+            // 
+            this.txtMiktar.Location = new System.Drawing.Point(143, 110);
+            this.txtMiktar.Name = "txtMiktar";
+            this.txtMiktar.Size = new System.Drawing.Size(154, 20);
+            this.txtMiktar.TabIndex = 2;
+            // 
+            // lblSonuc
+            // 
+            this.lblSonuc.AutoSize = true;
+            this.lblSonuc.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblSonuc.Location = new System.Drawing.Point(56, 185);
+            this.lblSonuc.Name = "lblSonuc";
+            this.lblSonuc.Size = new System.Drawing.Size(139, 24);
+            this.lblSonuc.TabIndex = 3;
+            this.lblSonuc.Text = "Ödenecek tutar";
+            // 
+            // btnHesapla
+            // 
+            this.btnHesapla.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnHesapla.Location = new System.Drawing.Point(331, 110);
+            this.btnHesapla.Name = "btnHesapla";
+            this.btnHesapla.Size = new System.Drawing.Size(75, 65);
+            this.btnHesapla.TabIndex = 4;
+            this.btnHesapla.Text = "Hesapla";
+            this.btnHesapla.UseVisualStyleBackColor = true;
+            this.btnHesapla.Click += new System.EventHandler(this.btnHesapla_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(370, 27);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "label2";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(445, 269);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.btnHesapla);
+            this.Controls.Add(this.lblSonuc);
+            this.Controls.Add(this.txtMiktar);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.tbTaksit);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.tbTaksit)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TrackBar tbTaksit;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtMiktar;
+        private System.Windows.Forms.Label lblSonuc;
+        private System.Windows.Forms.Button btnHesapla;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ track bar örnek/Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace track_bar_örnek
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void tbTaksit_ValueChanged(object sender, EventArgs e)
+        {
+            label2.Text= tbTaksit.Value.ToString(); 
+        }
+
+        private void btnHesapla_Click(object sender, EventArgs e)
+        {
+            int taksit, tutar;
+            double sonuc;
+            taksit =tbTaksit.Value;
+            tutar = Convert.ToInt32(txtMiktar.Text);
+
+            sonuc = (double)tutar / taksit;
+            lblSonuc.Text ="Taksitli tutar: " +sonuc.ToString(); 
+        }
+    }
+}
